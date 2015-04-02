e4a6e25d857c99f5e2010130f059279e7d496159
avidalh <avidalh@__home__gmail.com>
2015-04-02 16:56:44 +0100

    Full stack Nano Degree Project 1 Movie Trailer Web Generator.

---
+++ media.py
@@ -0,0 +1,23 @@
+import webbrowser
+
+
+class Movie():
+    """
+    Class movie constructor...
+    """
+
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, country, director, year):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.country = country
+        self.director = director
+        self.year = year
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+    def show_poster(self):
+        webbrowser.open(self.poster_image_url)
+
